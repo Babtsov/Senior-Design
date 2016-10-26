c7543eb74b4cadea9465bf94714e21d56b09c8de
Babtsov <ben.babtsov@uf.edu>
2016-10-26 01:25:44 -0400

    simplify init

--- GccApplication1/GccApplication1.c
+++ GccApplication1/GccApplication1.c
@@ -6,16 +6,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*************** LCD Configuration ****************/
-
-#define lcdDdr		DDRD		//Data direction 
-#define lcdPort		PORTD		//PortD
-#define lcdD7Bit	PORTD6		//LCD D7 (pin 14) -> PORTD6
-#define lcdD6Bit	PORTD5		//LCD D6 (pin 13) -> PORTD5
-#define lcdD5Bit	PORTD4		//LCD D5 (pin 12) -> PORTD4
-#define lcdD4Bit	PORTD3		//LCD D4 (pin 11) -> PORTD3               
-#define lcdEBit		PORTD2		//LCD E (pin 6)   -> PORTD2
-#define lcdRSBit	PORTD1		//LCD RS (pin 4)  -> PORTD1
+#define LCD_DIR		DDRD 
+#define LCD_PORT	PORTD          
+#define lcdEBit		PORTD2
+#define lcdRSBit	PORTD1
 
 #define lineOne			0x00                // Line 1
 #define lineTwo			0x40                // Line 2
@@ -28,72 +22,66 @@
 #define bit4Mode 		0b00101000          // we are using 4 bits of data
 #define setCursor       0b10000000          //sets the position of cursor
 
-void lcd_write(uint8_t);
-void lcd_instruction(uint8_t);
-void lcd_char(uint8_t);
-void lcd_string(char[]);
-void lcd_init(void);
-void adc_init(void);
-long adc_read(void);
+void LCD_send_upper_nibble(uint8_t);
+void LCD_command(uint8_t);
+void LCD_char(uint8_t);
+void LCD_string(char[]);
+void LCD_init(void);
 
+void LCD_init(void)
+{
+	LCD_DIR |= 0x7E; // Data: PORTD6..PORTD3, E: PORTD2, RS: PORTD1 
+    LCD_command(reset);                 
+    LCD_command(bit4Mode);
+    LCD_command(off);                               
+    LCD_command(clear);
+    LCD_command(entryMode);
+    LCD_command(on);
+}
+
+void LCD_string(char string[])
+{
+    for (int i = 0; string[i] != 0; i++)
+    {
+        LCD_char(string[i]);
+    }
+}
+
+void LCD_char(uint8_t data)
+{
+    LCD_PORT |= (1 << lcdRSBit);                 // RS high
+    LCD_PORT &= ~(1 << lcdEBit);                // E low
+    LCD_send_upper_nibble(data);                          // write the upper four bits of data
+	_delay_us(10);
+    LCD_send_upper_nibble(data << 4);                    // write the lower 4 bits of data
+	_delay_us(10);
+}
+
+void LCD_command(uint8_t cmd)
+{
+    LCD_PORT &= ~(1 << lcdRSBit); 
+    LCD_PORT &= ~(1 << lcdEBit); 
+    LCD_send_upper_nibble(cmd); 
+	_delay_us(10);
+    LCD_send_upper_nibble(cmd << 4);
+	_delay_ms(10);
+}
+
+void LCD_send_upper_nibble(uint8_t byte)
+{
+	LCD_PORT &= ~0x78; // Save the data of the LCD port (& set nibble to 0)
+	LCD_PORT |= byte >> 1 & 0x78; // set the nibble (requires shifting)
+    LCD_PORT |= (1 << lcdEBit);
+    LCD_PORT &= ~(1 << lcdEBit);
+}
 
 int main(void)
 {
-    lcd_init(); 
-	lcd_string("Eminem!!!!");	
+	LCD_init();
+	LCD_string("say my name");
 	while(1)
 	{
 	}
 	
-    return 0;
-}
-
-void lcd_init(void)
-{
-	lcdDdr |= (1 << lcdD7Bit) | (1 << lcdD6Bit) | (1 << lcdD5Bit) | (1 << lcdD4Bit) | (1 << lcdEBit) | (1 << lcdRSBit);
-    lcd_instruction(reset);                 
-    lcd_instruction(bit4Mode);
-    lcd_instruction(off);                               
-    lcd_instruction(clear);
-    lcd_instruction(entryMode);
-    lcd_instruction(on);
-}
-
-
-void lcd_string(char string[])
-{
-    for (int i = 0; string[i] != 0; i++)
-    {
-        lcd_char(string[i]);
-    }
-}
-
-
-void lcd_char(uint8_t data)
-{
-    lcdPort |= (1 << lcdRSBit);                 // RS high
-    lcdPort &= ~(1 << lcdEBit);                // E low
-    lcd_write(data);                          // write the upper four bits of data
-	_delay_us(2);
-    lcd_write(data << 4);                    // write the lower 4 bits of data
-	_delay_us(2);
-}
-
-
-void lcd_instruction(uint8_t instruction)
-{
-    lcdPort &= ~(1 << lcdRSBit); 
-    lcdPort &= ~(1 << lcdEBit); 
-    lcd_write(instruction); 
-	_delay_us(2);
-    lcd_write(instruction << 4);
-	_delay_ms(2);
-}
-
-void lcd_write(uint8_t byte)
-{
-	lcdPort &= ~0x78;
-	lcdPort |= byte >> 1 & 0x78;
-    lcdPort |= (1 << lcdEBit);
-    lcdPort &= ~(1 << lcdEBit);
+	return 0;
 }
