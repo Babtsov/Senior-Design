05a6519d2e6a7f6c9f95884ab4ba0c72d0aad898
Babtsov <ben.babtsov@ufl.edu>
2016-10-27 07:41:35 -0400

    Added push button for in between scanning

--- GccApplication1/GccApplication1.c
+++ GccApplication1/GccApplication1.c
@@ -22,6 +22,7 @@ void LCD_send_upper_nibble(uint8_t);
 void LCD_command(uint8_t);
 void LCD_char(uint8_t);
 void LCD_string(char[]);
+void LCD_substring(char[],int,int);
 void LCD_init(void);
 
 void LCD_init(void) {
@@ -35,6 +36,11 @@ void LCD_init(void) {
 
 void LCD_string(char string[]) {
     for (int i = 0; string[i] != 0; i++) {
+        LCD_char(string[i]);
+    }
+}
+void LCD_substring(char string[], int begin, int end) {
+    for (int i = begin; string[i] != 0 && i < end; i++) {
         LCD_char(string[i]);
     }
 }
@@ -66,12 +72,12 @@ void LCD_send_upper_nibble(uint8_t byte) {
 
 #define BAUDRATE 25 // baud rate: 2400 (see pg. 168)
 void UART_init(void) {
-    PORTD |= 0x01; // Enable transmitter
+    PORTD |= 0x01; // enable transmitter
     UBRRH = (BAUDRATE>>8);
     UBRRL = BAUDRATE;
-    UCSRB = (1<<TXEN)|(1<<RXEN);
-    UCSRC = (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1);   // 8bit data format 1 parity
-    UCSRB |= (1 << RXCIE); 
+    UCSRB = (1<<TXEN) | (1<<RXEN);
+    UCSRC = (1<<URSEL) | (1<<UCSZ0) | (1<<UCSZ1);   // 8bit data format 1 parity
+    UCSRB |= (1 << RXCIE); // enable interrupt on receive
 }
 
 void UART_send(unsigned char data) {
@@ -84,52 +90,61 @@ unsigned char UART_get_char(void) {
     return UDR;
 }
 
-#define CREADER_BUFF_SIZE 15		// card reader buffer size
+#define CREADER_BUFF_SIZE 12 // card reader buffer size
 struct {
     volatile char ID_str[CREADER_BUFF_SIZE + 1]; //extra char for null terminator
-    volatile uint8_t index; // ptr to an unoccupied 
-    volatile bool locked; // is the buffer ready to be consumed?
+    volatile uint8_t index; // pointer to an unoccupied slot
+    volatile bool locked; // buffer is locked from modifications by the ISR
 } creader_buff;
 
 
 inline void waitfor_creader_buff(void) {
-    while (!creader_buff.locked); // wait until buffer is locked
+    while (!creader_buff.locked); // wait until buffer is unlocked for consumption
 }
 
 inline void release_creader_buff(void) {
     creader_buff.locked = false;
 }
 
-ISR(USART_RXC_vect)
-{
+ISR(USART_RXC_vect) {
     char c = UART_get_char();
-    if (!creader_buff.locked) { // modify buffer if not locked
-        creader_buff.ID_str[creader_buff.index++] = c;
-        if (creader_buff.index >= 12) {
-            creader_buff.index = 0;
-            creader_buff.locked = true; // lock the buffer so it won't be modified until consumed
-        }
-    }
     UART_send(c);
+    if (creader_buff.locked) {
+        return;
+    }
+    uint8_t index = creader_buff.index;
+    if ((index == 0 && c != 0x0A) || (index == CREADER_BUFF_SIZE - 1 && c != 0x0D)) { 
+        creader_buff.index = 0; // reset buffer since data is not valid
+        return;
+    }
+    creader_buff.ID_str[creader_buff.index++] = c;
+    if (creader_buff.index >= CREADER_BUFF_SIZE) {
+        creader_buff.index = 0;
+        creader_buff.ID_str[CREADER_BUFF_SIZE] = 0; // null terminator for sanity
+        creader_buff.locked = true; // lock the buffer so it won't be modified until consumed by user
+    }
 }
 
 struct card {
-    char id[CREADER_BUFF_SIZE];
+    char id[CREADER_BUFF_SIZE + 1];
 } cards[2];
 
 void scan_cards(void) {
     for (int i = 0; i < 2; i++) {
         LCD_command(home);
+        LCD_command(clear);
         LCD_string("Scan card ");
         LCD_char(i + '1');
         LCD_string(":");
         waitfor_creader_buff();
-        strcpy(cards[i].id,(char *)creader_buff.ID_str);
+        strcpy(cards[i].id, (char *)creader_buff.ID_str);
         LCD_command(setCursor | lineTwo);
-        LCD_string(cards[i].id);
+        LCD_substring(cards[i].id, 1, CREADER_BUFF_SIZE - 1);
+        while (!(PIND & (1<<PD2))); // wait until user presses button
         release_creader_buff();
     }
 }
+
 int find_card(char * str) {
     for (int i = 0; i < 2; i++) {
         if (strcmp(cards[i].id, str) == 0) {
@@ -154,8 +169,10 @@ int main(void) {
             LCD_string("card ");
             LCD_char(card_index + '1');
             LCD_string(" detected!");
+            LCD_command(setCursor | lineTwo);
+            LCD_substring(cards[card_index].id, 1, CREADER_BUFF_SIZE - 1);
         } else {
-            LCD_string("invalid card?");
+            LCD_string("Unregistered Card.");
         }
         release_creader_buff();
     }
